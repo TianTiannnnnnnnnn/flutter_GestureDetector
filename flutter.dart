3073ddd78effb9e06548e7ca7cd017d3b5e9faa2
TianTiannnnnnnnnn <137465219+TianTiannnnnnnnnn@users.noreply.github.com>
2024-10-03 11:31:29 +0800

    Add files via upload

---
+++ flutter.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+// Gestures Detector
+
+class MyButton extends StatelessWidget {
+  const MyButton ({super.key});
+
+  @override
+  Widget build (BuildContext context) {
+    
+    return GestureDetector (
+      onTap: () {
+        print('MyButton was Tapped!');
+
+      },
+      child: Container (
+        height: 50,
+        padding: const EdgeInsets.all(9),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.lightGreen[500],
+
+        ),
+        child: const Center (
+          child: Text('Engage'),
+        ),
+      )
+    );
+  }
+}
+
+void main ()
+{
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+
+        )
+      )
+    )
+  );
+}
